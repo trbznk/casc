7b632f72fdff2ffe78600229e2e8d62f2376683e
trbznk <alexander.terbeznik@icloud.com>
2024-05-10 22:01:55 +0200

    add enum tables, func node, symbols, split node string casting

--- casc.c
+++ casc.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define FIXED_STRING_SIZE 32
 
@@ -30,15 +31,31 @@ typedef enum {
     TOKEN_TYPE_COUNT
 } TokenType;
 
+static const char *token_type_string_table[] = {
+    "NUMBER",
+    "IDENTIFIER",
+    "PLUS",
+    "MINUS",
+    "STAR",
+    "SLASH",
+    "CARET",
+    "L_PAREN",
+    "R_PAREN",
+    "EOF"
+};
+_Static_assert(sizeof(token_type_string_table)/sizeof(token_type_string_table[0]) == TOKEN_TYPE_COUNT, "");
+
 typedef struct {
     TokenType type;
-    char lexeme[FIXED_STRING_SIZE];
+
+    // TODO: I think we don't want to have fixed strings here in general
+    char text[FIXED_STRING_SIZE];
 } Token;
 
 typedef struct {
     size_t size;
     size_t capacity;
-    Token *data;    
+    Token *data;
 } Tokens;
 
 typedef enum {
@@ -53,71 +70,73 @@ typedef enum {
     OP_TYPE_COUNT
 } OpType;
 
+static const char *op_type_string_table[] = {
+    "Add",
+    "Sub",
+    "Mul",
+    "Div",
+    "UAdd",
+    "USub"
+};
+_Static_assert(sizeof(op_type_string_table)/sizeof(op_type_string_table[0]) == OP_TYPE_COUNT, "");
+
+static const char op_type_char_table[] = {
+    '+',
+    '-',
+    '*',
+    '/',
+    '+',
+    '-'
+};
+_Static_assert(sizeof(op_type_char_table)/sizeof(op_type_char_table[0]) == OP_TYPE_COUNT, "");
+
 typedef enum {
     NODE_INTEGER,
+    NODE_SYMBOL,
     NODE_BINOP,
     NODE_UNARYOP,
+    NODE_FUNC,
 
     NODE_TYPE_COUNT
 } NodeType;
+
+static const char *node_type_string_table[] = {
+    "Integer",
+    "Symbol",
+    "BinOp",
+    "UnaryOp",
+    "Func"
+};
+_Static_assert(sizeof(node_type_string_table)/sizeof(node_type_string_table[0]) == NODE_TYPE_COUNT, "");
+
+typedef enum {
+    FUNC_SQRT,
+
+    FUNC_TYPE_COUNT
+} FuncType;
+
+static const char *func_type_string_table[] = {
+    "sqrt"
+};
+_Static_assert(sizeof(func_type_string_table)/sizeof(func_type_string_table[0]) == FUNC_TYPE_COUNT, "");
 
 struct Node {
     Node* left;
     Node* right;
     Node* expr;
 
+    char name;
+    int value_i32;
+
     NodeType type;
     OpType op_type;
-    int value_i32;
+    FuncType func_type;
 };
 
 struct Parser {
     Tokens tokens;
     size_t pos;
 };
-
-const char* token_type_name(TokenType type) {
-    static const char* names[] = {
-        "NUMBER",
-        "IDENTIFIER",
-        "PLUS",
-        "MINUS",
-        "STAR",
-        "SLASH",
-        "CARET",
-        "L_PAREN",
-        "R_PAREN",
-        "EOF"
-    };
-    _Static_assert(sizeof(names)/sizeof(names[0]) == TOKEN_TYPE_COUNT, "ERROR: Wrong number of names.");
-    assert(type < TOKEN_TYPE_COUNT);
-    return names[type];
-}
-
-const char* op_type_name(OpType type) {
-    static const char* names[] = {
-        "Add",
-        "Sub",
-        "Mul",
-        "Div",
-        "UAdd",
-        "USub"
-    };
-    _Static_assert(sizeof(names)/sizeof(names[0]) == OP_TYPE_COUNT, "ERROR: Wrong number of names.");
-    assert(type < OP_TYPE_COUNT);
-    return names[type];
-}
-
-const char* node_type_name(NodeType type) {
-    static const char* names[] = {
-        "Integer",
-        "BinOp",
-        "UnaryOp"
-    };
-    _Static_assert(sizeof(names)/sizeof(names[0]) == NODE_TYPE_COUNT, "ERROR: Wrong number of names.");
-    assert(type < NODE_TYPE_COUNT);
-    return names[type];
-}
 
 void tokens_append(Tokens *tokens, Token token) {
     const size_t alloc_capacity = 10;
@@ -144,47 +163,57 @@ Tokens tokenize(char* source) {
                 i++;
             }
             Token token = { .type = TOKEN_NUMBER };
-            strcpy(token.lexeme, number_buffer);
+            strcpy(token.text, number_buffer);
             tokens_append(&tokens, token);
             i--;
         } else if (isalpha(source[i])) {
+            // maybe we need this code again when introducing identifier for function calls e.g. sin(2)
             char number_buffer[FIXED_STRING_SIZE] = "";
             size_t first = i;
             while (isalpha(source[i]) || (i > first && (isalpha(source[i]) || isdigit(source[i])))) {
                 strncat(number_buffer, &source[i], 1);
                 i++;
             }
-            Token token = { .type = TOKEN_IDENTIFIER };
-            strcpy(token.lexeme, number_buffer);
-            tokens_append(&tokens, token);
-            i--;
+            if (!strcmp(number_buffer, "sqrt")) {
+                Token token = { .type = TOKEN_IDENTIFIER };
+                strcpy(token.text, number_buffer);
+                tokens_append(&tokens, token);
+                i--;
+            } else {
+                i = first;
+                Token token = { .type = TOKEN_IDENTIFIER };
+                strncpy(token.text, &source[i], 1);
+                tokens_append(&tokens, token);
+            }
+
+
         } else if (source[i] == '+') {
             Token token = { .type = TOKEN_PLUS };
-            strncpy(token.lexeme, &source[i], 1);
+            strncpy(token.text, &source[i], 1);
             tokens_append(&tokens, token);
         } else if (source[i] == '-') {
             Token token = { .type = TOKEN_MINUS };
-            strncpy(token.lexeme, &source[i], 1);
+            strncpy(token.text, &source[i], 1);
             tokens_append(&tokens, token);
         } else if (source[i] == '*') {
             Token token = { .type = TOKEN_STAR };
-            strncpy(token.lexeme, &source[i], 1);
+            strncpy(token.text, &source[i], 1);
             tokens_append(&tokens, token);
         } else if (source[i] == '/') {
             Token token = { .type = TOKEN_SLASH };
-            strncpy(token.lexeme, &source[i], 1);
+            strncpy(token.text, &source[i], 1);
             tokens_append(&tokens, token);
         } else if (source[i] == '^') {
             Token token = { .type = TOKEN_CARET };
-            strncpy(token.lexeme, &source[i], 1);
+            strncpy(token.text, &source[i], 1);
             tokens_append(&tokens, token);
         } else if (source[i] == '(') {
             Token token = { .type = TOKEN_L_PAREN };
-            strncpy(token.lexeme, &source[i], 1);
+            strncpy(token.text, &source[i], 1);
             tokens_append(&tokens, token);
         } else if (source[i] == ')') {
             Token token = { .type = TOKEN_R_PAREN };
-            strncpy(token.lexeme, &source[i], 1);
+            strncpy(token.text, &source[i], 1);
             tokens_append(&tokens, token);
         } else if (isspace(source[i])) {
             // ignore spaces for now
@@ -199,17 +228,11 @@ Tokens tokenize(char* source) {
     return tokens;
 }
 
-Token parser_next_token(Parser* parser) {
-    Token next_token = parser->tokens.data[parser->pos];
-    parser->pos++;
-    return next_token;
-}
-
 void parser_expect(Parser* parser, TokenType type) {
     if (parser->tokens.data[parser->pos].type == type) {
         parser->pos++;
     } else {
-        fprintf(stderr, "ERROR: Expected %s got %s.", token_type_name(type), token_type_name(parser->tokens.data[parser->pos].type));
+        fprintf(stderr, "ERROR: Expected %s got %s.", token_type_string_table[type], token_type_string_table[parser->tokens.data[parser->pos].type]);
     }
 }
 
@@ -217,6 +240,13 @@ Node* ast_integer(int value) {
     Node* node = malloc(sizeof(Node));
     node->type = NODE_INTEGER;
     node->value_i32 = value;
+    return node;
+}
+
+Node* ast_symbol(char name) {
+    Node* node = malloc(sizeof(Node));
+    node->type = NODE_SYMBOL;
+    node->name = name;
     return node;
 }
 
@@ -237,33 +267,56 @@ Node* ast_unaryop(Node* expr, OpType op_type) {
     return node;
 }
 
+Node* ast_func(FuncType func_type, Node* expr) {
+    Node* node = malloc(sizeof(Node));
+    node->type = NODE_FUNC;
+    node->expr = expr;
+    node->func_type = func_type;
+    return node;
+}
+
 bool node_eq(Node* left, Node* right) {
     if (left->type == right->type) {
         switch (left->type) {
             case NODE_INTEGER:
                 return left->value_i32 == right->value_i32;
+            case NODE_SYMBOL:
+                return left->name == right->name;
             case NODE_BINOP:
                 return node_eq(left->left, right->left) && node_eq(left->right, right->right);
-            default: fprintf(stderr, "ERROR: node type %s not implemented.\n", node_type_name(left->type)); exit(1);
+            case NODE_FUNC:
+                return left->func_type == right->func_type && node_eq(left->expr, right->expr);
+            default: fprintf(stderr, "ERROR: Cannot do 'node_eq' because node type '%s' is not implemented.\n", node_type_string_table[left->type]); exit(1);
         }
     }
     return false;
 }
 
 Node* parse_factor(Parser* parser) {
-    Token token = parser->tokens.data[parser->pos];
-    if (token.type == TOKEN_NUMBER) {
+    Token current_token = parser->tokens.data[parser->pos];
+    if (current_token.type == TOKEN_NUMBER) {
         parser_expect(parser, TOKEN_NUMBER);
-        return ast_integer(atoi(token.lexeme));   
-    } else if (token.type == TOKEN_L_PAREN) {
+        return ast_integer(atoi(current_token.text));
+    } else if (current_token.type == TOKEN_IDENTIFIER) {
+        if (!strcmp(current_token.text, "sqrt")) {
+            parser_expect(parser, TOKEN_IDENTIFIER);
+            parser_expect(parser, TOKEN_L_PAREN);
+            Node* body = parse_expr(parser);
+            parser_expect(parser, TOKEN_R_PAREN);
+            return ast_func(FUNC_SQRT, body);
+        } else {
+            parser_expect(parser, TOKEN_IDENTIFIER);
+            return ast_symbol(current_token.text[0]);
+        }
+    } else if (current_token.type == TOKEN_L_PAREN) {
         parser_expect(parser, TOKEN_L_PAREN);
         Node* r = parse_expr(parser);
         parser_expect(parser, TOKEN_R_PAREN);
         return r;
-    } else if (token.type == TOKEN_MINUS) {
+    } else if (current_token.type == TOKEN_MINUS) {
         parser_expect(parser, TOKEN_MINUS);
         return ast_unaryop(parse_factor(parser), OP_USUB);
-    } else if (token.type == TOKEN_PLUS) {
+    } else if (current_token.type == TOKEN_PLUS) {
         parser_expect(parser, TOKEN_PLUS);
         return ast_unaryop(parse_factor(parser), OP_UADD);
     } else {
@@ -273,13 +326,16 @@ Node* parse_factor(Parser* parser) {
 
 Node* parse_term(Parser* parser) {
     Node* result = parse_factor(parser);
-    while (parser->tokens.data[parser->pos].type == TOKEN_STAR || parser->tokens.data[parser->pos].type == TOKEN_SLASH) {
+
+    while (parser->tokens.data[parser->pos].type == TOKEN_STAR || parser->tokens.data[parser->pos].type == TOKEN_SLASH || parser->tokens.data[parser->pos].type == TOKEN_IDENTIFIER) {
         if (parser->tokens.data[parser->pos].type == TOKEN_STAR) {
             parser_expect(parser, TOKEN_STAR);
             result = ast_binop(result, parse_factor(parser), OP_MUL);
         } else if (parser->tokens.data[parser->pos].type == TOKEN_SLASH) {
             parser_expect(parser, TOKEN_SLASH);
             result = ast_binop(result, parse_factor(parser), OP_DIV);
+        } else if (parser->tokens.data[parser->pos].type == TOKEN_IDENTIFIER) {
+            result = ast_binop(result, parse_factor(parser), OP_MUL);
         }
     }
     return result;
@@ -302,14 +358,32 @@ Node* parse_expr(Parser* parser) {
     return result;
 }
 
-char* node_to_string(Node* node) {
+char *node_to_debug_string(Node* node) {
     // TODO: memory management needed here
-    char* output = malloc(1024);
+    char *output = malloc(1024);
     switch (node->type) {
-        case NODE_INTEGER: sprintf(output, "%s(%d)", node_type_name(node->type), node->value_i32); break;
-        case NODE_BINOP: sprintf(output, "%s(%s, %s)", op_type_name(node->op_type), node_to_string(node->left), node_to_string(node->right)); break;
-        case NODE_UNARYOP: sprintf(output, "%s(%s)", op_type_name(node->op_type), node_to_string(node->expr)); break;
-        default: fprintf(stderr, "ERROR: node type %s not implemented.\n", node_type_name(node->type)); exit(1);
+        case NODE_INTEGER: sprintf(output, "%s(%d)", node_type_string_table[node->type], node->value_i32); break;
+        case NODE_SYMBOL: sprintf(output, "%s(%c)", node_type_string_table[node->type], node->name); break;
+        case NODE_BINOP: sprintf(output, "%s(%s, %s)", op_type_string_table[node->op_type], node_to_debug_string(node->left), node_to_debug_string(node->right)); break;
+        case NODE_UNARYOP: sprintf(output, "%s(%s)", op_type_string_table[node->op_type], node_to_debug_string(node->expr)); break;
+        case NODE_FUNC: sprintf(output, "%s(%s)", func_type_string_table[node->func_type], node_to_debug_string(node->expr)); break;
+        default: fprintf(stderr, "ERROR: Cannot do 'node_to_debug_string' because node type '%s' is not implemented.\n", node_type_string_table[node->type]); exit(1);
+    }
+    return output;
+}
+
+char *node_to_string(Node* node) {
+    char *output = malloc(1024);
+    switch (node->type) {
+        case NODE_INTEGER: sprintf(output, "%d", node->value_i32); break;
+        case NODE_SYMBOL: sprintf(output, "%c", node->name); break;
+        case NODE_BINOP: {
+            sprintf(output, "(%s%c%s)", node_to_string(node->left), op_type_char_table[node->op_type], node_to_string(node->right));
+            break;
+        }
+        case NODE_UNARYOP: sprintf(output, "%c%s", op_type_char_table[node->op_type], node_to_string(node->expr)); break;
+        case NODE_FUNC: sprintf(output, "%s(%s)", func_type_string_table[node->func_type], node_to_string(node->expr)); break;
+        default: fprintf(stderr, "ERROR: Cannot do 'node_to_debug_string' because node type '%s' is not implemented.\n", node_type_string_table[node->type]); exit(1);
     }
     return output;
 }
@@ -378,67 +452,102 @@ Node* interp(Node* node) {
             return interp_unaryop(node);
         case NODE_INTEGER:
             return node;
+        case NODE_FUNC: {
+            if (node->expr->type == NODE_INTEGER && node->func_type == FUNC_SQRT) {
+                double result = sqrt((double)node->expr->value_i32);
+                if (fmod((double)node->expr->value_i32, result) == 0.0) {
+                    return ast_integer((int)result);
+                }
+            }
+            return node;
+        }
         default:
-            fprintf(stderr, "ERROR: Cannot interpretate node type %s.\n", node_type_name(node->type));
+            fprintf(stderr, "ERROR: Cannot interp node type '%s'\n", node_type_string_table[node->type]);
             exit(1);
     }
 }
 
 void test() {
-    size_t id = 0;
-    #define TEST_CASE(source, test_case) {{ \
+    size_t test_id = 0;
+
+    #define TEST_SOURCE_TO_PARSE(source, test_case) {{ \
+        Tokens tokens = tokenize(source); \
+        Parser parser = { .tokens = tokens, .pos=0 }; \
+        Node* ast = parse_expr(&parser); \
+        printf("test %02zu...........................", test_id);\
+        if (!node_eq(ast, test_case)) {\
+            printf("failed\n");\
+            exit(1); \
+        } else { \
+            printf("passed\n");\
+        }\
+        test_id++;\
+    }}
+
+    #define TEST_SOURCE_TO_INTERP(source, test_case) {{ \
         Tokens tokens = tokenize(source); \
         Parser parser = { .tokens = tokens, .pos=0 }; \
         Node* ast = parse_expr(&parser); \
         Node* output = interp(ast); \
-        printf("test %02zu...........................", id);\
+        printf("test %02zu...........................", test_id);\
         if (!node_eq(output, test_case)) {\
             printf("failed\n");\
             exit(1); \
         } else { \
             printf("passed\n");\
         }\
-        id++;\
+        test_id++;\
     }}
-    TEST_CASE("27 + 3", ast_integer(30));
-    TEST_CASE("27 - 7", ast_integer(20));
-    TEST_CASE("7 - 3 + 2 - 1", ast_integer(5));
-    TEST_CASE("10 + 1 + 2 - 3 + 4 + 6 - 15", ast_integer(5));
-    TEST_CASE("7 * 4 / 2", ast_integer(14));
-    TEST_CASE("7 * 4 / 2 * 3", ast_integer(42));
-    TEST_CASE("10 * 4  * 2 * 3 / 8", ast_integer(30));
-    TEST_CASE("7 - 8 / 4", ast_integer(5));
-    TEST_CASE("14 + 2 * 3 - 6 / 2", ast_integer(17));
-    TEST_CASE("7 + 3 * (10 / (12 / (3 + 1) - 1))", ast_integer(22));
-    TEST_CASE("7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)", ast_integer(10));
-    TEST_CASE("7 + (((3 + 2)))", ast_integer(12));
-    TEST_CASE("- 3", ast_integer(-3));
-    TEST_CASE("+ 3", ast_integer(3));
-    TEST_CASE("5 - - - + - 3", ast_integer(8));
-    TEST_CASE("5 - - - + - (3 + 4) - +2", ast_integer(10));
+
+    // ruslanspivak interpreter tutorial tests
+    TEST_SOURCE_TO_INTERP("27 + 3", ast_integer(30));
+    TEST_SOURCE_TO_INTERP("27 - 7", ast_integer(20));
+    TEST_SOURCE_TO_INTERP("7 - 3 + 2 - 1", ast_integer(5));
+    TEST_SOURCE_TO_INTERP("10 + 1 + 2 - 3 + 4 + 6 - 15", ast_integer(5));
+    TEST_SOURCE_TO_INTERP("7 * 4 / 2", ast_integer(14));
+    TEST_SOURCE_TO_INTERP("7 * 4 / 2 * 3", ast_integer(42));
+    TEST_SOURCE_TO_INTERP("10 * 4  * 2 * 3 / 8", ast_integer(30));
+    TEST_SOURCE_TO_INTERP("7 - 8 / 4", ast_integer(5));
+    TEST_SOURCE_TO_INTERP("14 + 2 * 3 - 6 / 2", ast_integer(17));
+    TEST_SOURCE_TO_INTERP("7 + 3 * (10 / (12 / (3 + 1) - 1))", ast_integer(22));
+    TEST_SOURCE_TO_INTERP("7 + 3 * (10 / (12 / (3 + 1) - 1)) / (2 + 3) - 5 - 3 + (8)", ast_integer(10));
+    TEST_SOURCE_TO_INTERP("7 + (((3 + 2)))", ast_integer(12));
+    TEST_SOURCE_TO_INTERP("- 3", ast_integer(-3));
+    TEST_SOURCE_TO_INTERP("+ 3", ast_integer(3));
+    TEST_SOURCE_TO_INTERP("5 - - - + - 3", ast_integer(8));
+    TEST_SOURCE_TO_INTERP("5 - - - + - (3 + 4) - +2", ast_integer(10));
+
+    // things from the sympy tutorial
+    TEST_SOURCE_TO_PARSE("sqrt(3)", ast_func(FUNC_SQRT, ast_integer(3)));
+
+    // misc
+    TEST_SOURCE_TO_INTERP("sqrt(9)", ast_integer(3));
+}
+
+void tokens_print(Tokens *tokens) {
+    for (size_t i = 0; i < tokens->size; i++) {
+        Token *token = &tokens->data[i];
+        printf("%s('%s') ", token_type_string_table[token->type], token->text);
+    }
+    printf("\n");
 }
 
 int main() {
-    test();
 
-    char *source = "7 * 4 / 2 * 3";
-    printf("source................\n'%s'\n", source);
+#if 1
+    test();
+#endif
+
+    // char *source = "sqrt(8)";
+    char *source = "sqrt(9)";
     Tokens tokens = tokenize(source);
+    tokens_print(&tokens);
+
     Parser parser = { .tokens = tokens, .pos=0 };
     Node* ast = parse_expr(&parser);
-
-    // print tokens
-    printf("tokens................\n");
-    for (size_t i = 0; i < tokens.size; i++) {
-        Token *token = &tokens.data[i];
-        printf("%s('%s') ", token_type_name(token->type), token->lexeme);
-    }
-    printf("\n");
-    printf("ast................\n");
     printf("%s\n", node_to_string(ast));
-    // printf("lexer.size=%zu, lexer.capacity=%zu\n", lexer.size, lexer.capacity);
 
-    printf("output................\n");
     Node* output = interp(ast);
+    printf("%s\n", node_to_debug_string(output));
     printf("%s\n", node_to_string(output));
 }
